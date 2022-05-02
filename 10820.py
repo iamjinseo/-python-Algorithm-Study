b724a758d88cb352f8a9ef1049e83dde40780a4a
iamjinseo <58293743+iamjinseo@users.noreply.github.com>
2022-05-02 17:50:20 +0900

    문자열 분석 (끝없는 입력에 대한 try except)

--- 10820.py
+++ 10820.py
@@ -8,27 +8,26 @@
 while True:
     try:
         str = input()
+        lower , upper, num, blank = 0,0,0,0
+        for letter in str:
+            asciiNum = ord(letter) #문자 하나의 아스키코드
+            #공백
+            if asciiNum == 32:
+                blank+=1
+            #숫자
+            elif asciiNum>=48 and asciiNum<=57:
+                num+=1
+            #대문자
+            elif asciiNum >=65 and asciiNum<=90: 
+                upper+=1
+            #소문자
+            elif asciiNum >= 97 and asciiNum <= 122:
+                lower+=1
+        print(lower, upper, num, blank)
     except EOFError:
         break
 
-#소문자, 대문자, 숫자, 공백 검사
-for letter in str:
-    asciiNum = ord(letter) #문자 하나의 아스키코드
 
-    #공백
-    if asciiNum == 32:
-        output[index][3]+=1
-    #숫자
-    elif asciiNum>=48 and asciiNum<=57:
-        output[index][2]+=1
-    #대문자
-    elif asciiNum >=65 and asciiNum<=90: 
-        output[index][1]+=1
-    #소문자
-    elif asciiNum >= 97 and asciiNum <= 122:
-        output[index][0]+=1
-        
-
-#이중리스트 출력
-for x, y, z, q in output:
-    print(x, y, z, q)
+#배운 점
+#끝없이 입력하기때문에 try except를 해야하는구나...
+#굳이 이중리스트를 만들어서 각 문자열에 해당하는 소문자,대문자,공백,숫자 갯수를 일일이 세서 넣을 필요 없이 그때그때 구해서 출력하면 되는구나...
